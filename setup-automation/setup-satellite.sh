a5b43d8204f1c23d783056b6385c16fb47e8b918
Matthew Yee <myee@redhat.com>
2025-07-04 10:44:49 -0700

    fixed wget in satellite setup script

--- setup-automation/setup-satellite.sh
+++ setup-automation/setup-satellite.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This file is needed for challenge 5.
-wget https://github.com/myee111/hello-rpm/releases/download/0.0.1/hello-0.0.1-1.el9.noarch.rpm
+wget -P /root https://github.com/myee111/hello-rpm/releases/download/0.0.1/hello-0.0.1-1.el9.noarch.rpm
